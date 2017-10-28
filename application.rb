2bf915c2a05cc53b4358ae57521fd25546c4a34f
MatteBru <mbruni616@gmail.com>
2017-10-28 19:30:22 -0400

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,25 @@
 class Application
+  attr_accessor :hello_string
+
+  @hello_string = "Hello, my name is Matteo Bruni and this is a really crappy lil' webapp that nobody will see but me"
+
+  def string_baby_string(str)
+    str
+  end
+
+  def numtastic
+    i = 0
+    until i == 100
+      puts i
+      i += 1
+    end
+  end
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Matteo Bruni and this is a really crappy lil' webapp that nobody will see but me"
+    # resp.write numtastic
     resp.finish
   end
 
 end
-
